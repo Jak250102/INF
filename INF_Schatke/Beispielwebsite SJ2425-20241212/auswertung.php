8090a32e644ad13bb1ef911f553bd49a7d34fe50
Jak250102 <Jak250102@gmail.com>
2024-12-12 07:30:52 +0100

    Schatke zeug

---
+++ INF_Schatke/Beispielwebsite SJ2425-20241212/auswertung.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Ihre eingegebenen Daten:</h1> 
+
+<?php
+echo "<h1>Hello World</h1>";
+echo $_GET["nachname"];
+echo "<br>";
+echo $_GET["pw"];
+
+
+
+?>
+</body>
+</html>
