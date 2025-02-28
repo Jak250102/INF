6cf5618c630c75fb8fb93074316710cc8094960a
Jak250102 <Jak250102@gmail.com>
2025-02-28 14:30:44 +0100

    th

--- Python/test.py
+++ Python/test.py
@@ -17,17 +17,19 @@ def main():
 	root = tk.Tk()
 	root.title("Test")
 	root.eval('tk::PlaceWindow . center')
-	root.config(width=454, height=100)
+	root.config(width=454, height=100, background="grey")
 
 
 	# Erstelle einen Rahmen (Frame) zur Zentrierung
 	frame = tk.Frame(root)
-	frame.pack(expand=True ,)
+	frame.pack(expand=True)
+	frame.config(background="black", width=70)
+
 
 
 	# Erstelle ein Eingabefeld
 	entry = tk.Entry(frame)
-	entry.pack(pady=10)  # Abstand nach oben und unten
+	entry.pack(pady=10, padx=10 )  # Abstand nach oben und unten
 	entry.config(width=50, background="white", foreground="black")
 
 	# Funktion zur Anzeige der Eingabe
